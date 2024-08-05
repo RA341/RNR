ef361b1d96bdd19237fadb92b0e7acd2fc31831a
RA341 <razerishi1505@gmail.com>
2024-08-05 12:31:23 -0400

    app search ready

--- lib/presentation/home/installed_apps.dart
+++ lib/presentation/home/installed_apps.dart
@@ -7,39 +7,85 @@ class InstalledApps extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final installedApps = ref.watch(installedAppsProvider);
+    final installedApps = ref.watch(searchAppsAsyncProvider);
 
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(15),
-        child: installedApps.when(
-          data: (data) {
-            return ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (context, index) {
-                final e = data[index];
-
-                return Row(
-                  children: [
-                    if (e.icon != null)
-                      SizedBox(
-                        height: 40,
-                        width: 40,
-                        child: Image.memory(e.icon!),
-                      ),
-                    Text('${e.packageName} ${e.versionName}'),
-                  ],
-                );
+    return Flexible(
+      flex: 2,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+            child: SearchBar(
+              enabled: installedApps.hasValue,
+              hintText: 'Search apps',
+              onChanged: (value) {
+                ref.read(queryProvider.notifier).state = value;
               },
-            );
-          },
-          error: (error, stackTrace) {
-            return ErrorWidget(error);
-          },
-          loading: () {
-            return const CircularProgressIndicator();
-          },
-        ),
+              // trailing: [
+              //   IconButton(
+              //     onPressed: () {
+              //       ref.read(queryProvider.notifier).state = '';
+              //     },
+              //     icon: const Icon(Icons.cancel_outlined),
+              //   ),
+              // ],
+              leading: const Icon(Icons.search_outlined),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: installedApps.when(
+                data: (data) {
+                  return ListView.builder(
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      final e = data[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Row(
+                          children: [
+                            if (e.icon != null)
+                              SizedBox(
+                                height: 40,
+                                width: 40,
+                                child: Image.memory(e.icon!),
+                              ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 15),
+                              child: SizedBox(
+                                width: 250,
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      '${e.name} v${e.versionName}',
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    Text(
+                                      e.packageName,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+                error: (error, stackTrace) {
+                  return Center(child: ErrorWidget(error));
+                },
+                loading: () {
+                  return const Center(child: CircularProgressIndicator());
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/providers/installed_apps_provider.dart
+++ lib/providers/installed_apps_provider.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:installed_apps/app_info.dart';
 import 'package:rnr/services/app_manager.dart';
@@ -6,44 +5,43 @@ import 'package:rnr/utils/services.dart';
 
 final installedAppsProvider = FutureProvider<List<AppInfo>>((ref) async {
   return appMan.getAllApps();
-}
-);
+});
 
 final queryProvider = StateProvider<String>((ref) {
   return '';
 });
 
-final searchProvider = Provider<List<AppInfo>>((ref) {
+final searchAppsAsyncProvider = FutureProvider<List<AppInfo>>((ref) async {
   try {
-    final apps = ref
-        .watch(installedAppsProvider)
-        .requireValue;
+    final apps = await ref.watch(installedAppsProvider.future);
     final query = ref.watch(queryProvider);
+    if (query.isEmpty) {
+      return apps;
+    }
+
     return searchApps(query, apps);
   } catch (e) {
     logger
       ..e(e)
       ..d(
         'Search provider errored out, this is expected '
-            'probably trying to read searchProvider before results are retrieved',);
+        'probably trying to read searchProvider before results are retrieved',
+      );
     return [];
   }
 });
 
 List<AppInfo> searchApps(String query, List<AppInfo> apps) {
-  if (query.isEmpty) {
-    return [];
-  }
-
-  // no results when no apps present
+  assert(
+      query.isNotEmpty, 'Query should not be empty, if (searchApps) is called');
 
   final results = <AppInfo>[];
 
-  // for (final app in apps) {
-  //   if (app.packageName.contains(query) || app.appName.contains(query)) {
-  //     results.add(app);
-  //   }
-  // }
+  for (final app in apps) {
+    if (app.packageName.contains(query) || app.name.contains(query)) {
+      results.add(app);
+    }
+  }
 
   return results;
 }
